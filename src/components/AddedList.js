a2054c4a889961b5938f418791bff4e1739c79f0
Nick Foden <nickfoden@gmail.com>
2020-07-26 11:27:25 -0400

    Destructure an array meals and add default

--- src/components/AddedList.js
+++ src/components/AddedList.js
@@ -1,17 +1,15 @@
 import React from "react";
 import { connect } from "react-redux";
 
-const AddedList = (props) => {
-  return (
-    <div>
-      {props.meals.map((meal) => (
-        <li
-          key={meal.uuid}
-        >{`${meal.foodItem} added, calories:${meal.calories}`}</li>
-      ))}
-    </div>
-  );
-};
+const AddedList = ({ meals = [] }) => (
+  <div>
+    {meals.map((meal) => (
+      <li
+        key={meal.uuid}
+      >{`${meal.foodItem} added, calories:${meal.calories}`}</li>
+    ))}
+  </div>
+);
 
 const mapStateToProps = (state) => ({
   meals: state.meals,
